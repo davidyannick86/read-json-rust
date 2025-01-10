320c07cb9f2885be063818aedaf33386b72d9192
DavidYannick <57354908+davidyannick86@users.noreply.github.com>
2025-01-10 07:20:32 +0100

    Implement serialization for Article and Paragraph structs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,54 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug)]
+// * Implement the Serialize and Deserialize traits for the Paragraph struct
+#[derive(Serialize, Deserialize)]
 struct Paragraph {
     name: String,
 }
 
-fn main() {
-    println!("Hello, world!");
+impl std::fmt::Debug for Paragraph {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Paragraph content is {{ name: {} }}", self.name)
+    }
+}
+
+// * Implement the Serialize and Deserialize traits for the Article struct
+#[derive(Serialize, Deserialize)]
+struct Article {
+    title: String,
+    author: String,
+    content: Vec<Paragraph>,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let json = r#"
+    {
+        "title": "My Article",
+        "author": "John Doe",
+        "content": [
+            {
+                "name": "Introduction"
+            },
+            {
+                "name": "Body"
+            },
+            {
+                "name": "Conclusion"
+            }
+        ]
+    }"#;
+
+    let article = read_json_typed(json)?;
+
+    article.content.iter().for_each(|p| {
+        println!("{:?}", p);
+    });
+
+    Ok(())
+}
+
+fn read_json_typed(json: &str) -> Result<Article, Box<dyn std::error::Error>> {
+    let article: Article = serde_json::from_str(json)?;
+
+    Ok(article)
 }
